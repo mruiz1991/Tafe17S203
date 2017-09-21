85c17abf158064551c63276d38d42d4770654f8a
Martin Ruiz <martin.ruiz2@student.tafesa.edu.au>
2017-09-21 15:00:43 +0930

    Created PersonalInfo page

--- Start-Finance-master/InstaRichie/Views/PersonalDetails.xaml.cs
+++ Start-Finance-master/InstaRichie/Views/PersonalDetails.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using StartFinance.Models;
+using Windows.UI.Popups;
+using SQLite.Net;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +25,109 @@ namespace StartFinance.Views
     /// </summary>
     public sealed partial class PersonalDetails : Page
     {
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+
         public PersonalDetails()
         {
             this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            /// Initializing a database
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            Results();
+        }
+
+             public void Results()
+        {
+            // Creating table
+            conn.CreateTable<PersonalInfo>();
+
+            /// Refresh Data
+            var query = conn.Table<PersonalInfo>();
+            PersonalInfoListView.ItemsSource = query.ToList();
+        }
+
+        private async void AddData(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (FirstNameTxtBx.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No First Name entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    conn.Insert(new Models.PersonalInfo()
+                    {
+                        FirstName = FirstNameTxtBx.Text,
+                        LastName = LastNameTxtBx.Text,
+                        DOB = DOBCal.Text,
+                        Gender = GenderTxtBx.Text,
+                        Email = EmailTxtBx.Text,
+                        Phone = PhoneTxtBx.Text
+
+                    });
+                    Results();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    MessageDialog dialog = new MessageDialog("You forgot to enter the Value or entered an invalid data", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (ex is SQLiteException)
+                {
+                    MessageDialog dialog = new MessageDialog("A Similar Personal Info record already exists, Try a different name", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+            }
+        }
+
+        private async void ClearFileds_Click(object sender, RoutedEventArgs e)
+        {
+            FirstNameTxtBx.Text = string.Empty;
+            LastNameTxtBx.Text = string.Empty;
+            DOBCal.Text = string.Empty;
+            GenderTxtBx.Text = string.Empty;
+            EmailTxtBx.Text = string.Empty;
+            PhoneTxtBx.Text = string.Empty;
+
+
+            MessageDialog ClearDialog = new MessageDialog("Cleared", "information");
+            await ClearDialog.ShowAsync();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Results();
+        }
+
+        private async void DeleteAccout_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string AccSelection = ((Models.PersonalInfo)PersonalInfoListView.SelectedItem).FirstName;
+                if (AccSelection == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    conn.CreateTable<PersonalInfo>();
+                    var query1 = conn.Table<PersonalInfo>();
+                    var query3 = conn.Query<PersonalInfo>("DELETE FROM PersonalInfo WHERE FirstName ='" + AccSelection + "'");
+                    PersonalInfoListView.ItemsSource = query1.ToList();
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                await dialog.ShowAsync();
+            }
         }
     }
 }
