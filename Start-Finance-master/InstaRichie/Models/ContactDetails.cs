0c3520c71150863678f5202133823922f615c44d
Berat <uberaty@gmail.com>
2017-09-21 14:40:52 +0930

    Contact Details section is added and it is working

---
+++ Start-Finance-master/InstaRichie/Models/ContactDetails.cs
@@ -0,0 +1,24 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    public class ContactDetails
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [NotNull]
+        public string FirstName { get; set; }
+
+        [NotNull]
+        public string LastName { get; set; }
+
+        [NotNull]
+        public int Phone { get; set; }
+    }
+}
