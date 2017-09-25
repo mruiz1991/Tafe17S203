ba905ed66286e3d40cfdfcb404997223da80ef7e
Martin Ruiz <martin.ruiz2@student.tafesa.edu.au>
2017-09-25 13:05:31 +0930

    Added in personal details page with update, edit and delete

--- Start-Finance-master/InstaRichie/Views/PersonalDetails.xaml.cs
+++ Start-Finance-master/InstaRichie/Views/PersonalDetails.xaml.cs
@@ -129,5 +129,71 @@ namespace StartFinance.Views
                 await dialog.ShowAsync();
             }
         }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void EditData(object sender, RoutedEventArgs e)
+        {
+            MessageDialog ShowConf = new MessageDialog("Are you sure you want to update the selected record", "Important");
+            ShowConf.Commands.Add(new UICommand("Yes, Update")
+            {
+                Id = 0
+            });
+            ShowConf.Commands.Add(new UICommand("Cancel")
+            {
+                Id = 1
+            });
+            ShowConf.DefaultCommandIndex = 0;
+            ShowConf.CancelCommandIndex = 1;
+
+            var result = await ShowConf.ShowAsync();
+            if ((int)result.Id == 0)
+            {
+                // checks if data is null else inserts
+                try
+                {
+                    string PersonalFirstName = FirstNameTxtBx.Text;
+                    string PersonalLastName = LastNameTxtBx.Text;
+                    string PersonalDOB = DOBCal.Text;
+                    string PersonalGender = GenderTxtBx.Text;
+                    string PersonalEmail = EmailTxtBx.Text;
+                    string PersonalPhone = PhoneTxtBx.Text;
+                    int PersonalID = ((Models.PersonalInfo)PersonalInfoListView.SelectedItem).ID;
+
+                    if (PersonalFirstName == "")
+                    {
+                        MessageDialog dialog = new MessageDialog("Nothing selected", "Select an item");
+                        await dialog.ShowAsync();
+                    }
+                    else
+                    {
+                        conn.CreateTable<Models.PersonalInfo>();
+                        var query1 = conn.Table<Models.PersonalInfo>();
+                        var query3 = conn.Query<Models.PersonalInfo>("UPDATE PersonalInfo SET FirstName ='" + PersonalFirstName + "', LastName ='" + PersonalLastName + "', DOB ='" + PersonalDOB + "', Gender ='" + PersonalGender + "', Email ='" + PersonalEmail + "', Phone ='" + PersonalPhone + "' WHERE ID = '" + PersonalID + "'");
+                        PersonalInfoListView.ItemsSource = query1.ToList();
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    MessageDialog ClearDialog = new MessageDialog("Please select item to update", "Opps..");
+                    await ClearDialog.ShowAsync();
+                    
+                }
+                 
+            }
+                
+
+            /*FirstNameTxtBx.Text = string.Empty;
+            LastNameTxtBx.Text = string.Empty;
+            DOBCal.Text = string.Empty;
+            GenderTxtBx.Text = string.Empty;
+            EmailTxtBx.Text = string.Empty;
+            PhoneTxtBx.Text = string.Empty;*/
+        }
     }
 }
